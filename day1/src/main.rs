2f4369210c76c7efd2c2fe86192700b47f7b6724
Jake Swanson <jakswa@gmail.com>
2021-12-01 00:21:33 -0500

    day one part 1 & 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let input = std::fs::read_to_string("input.txt")?;
+    let nums: Vec<i64> = input.lines().filter_map(|i| i.parse().ok()).collect();
+    // part1:
+    //println!("res: {}", nums.windows(2).filter(|i| i[0] < i[1]).count());
+    println!(
+        "res: {}",
+        nums.windows(4)
+            .filter(|i| i[0..3].iter().sum::<i64>() < i[1..4].iter().sum())
+            .count()
+    );
+    Ok(())
 }
+
