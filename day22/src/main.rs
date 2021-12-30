ce33222395bed332d3af0296a9dbbe51c321e949
Jake Swanson <jakswa@gmail.com>
2021-12-30 11:52:46 -0500

    re-tackling day22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -33,58 +33,61 @@ fn main() {
             )
         })
         .collect::<Vec<(usize, bool, [i32; 6])>>();
-
-    let mut prisms_on = vec![cmds[0].2];
-    'cmdloop: for cmd in cmds[1..].iter().filter(|cmd| cmd.1) {
-        let mut collide = vec![cmd.2];
-        for p in prisms_on.iter() {
-            for col in collide.clone().iter() {
-                match try_collide(*p, *col) {
-                    None => break 'cmdloop,
-                    Some(mut new_collide) => collide.append(&mut new_collide),
-                }
-            }
-        }
-        prisms_on.append(&mut collide);
-    }
-    println!("shit {}: {:?}", prisms_on.len(), prisms_on);
 }
 
-fn try_collide(station: [i32; 6], orbit: [i32; 6]) -> Option<Vec<[i32; 6]>> {
-    if is_disjointed(&station, &orbit) {
-        return Some(vec![]);
-    }
-    if swallows(&station, &orbit) {
-        return None;
-    }
-    let mut ret = vec![];
-    for (x1, x2) in cranges(station[0], station[1], orbit[0], orbit[1]).iter() {
-        for (y1, y2) in cranges(station[2], station[3], orbit[2], orbit[3]).iter() {
-            for (z1, z2) in cranges(station[4], station[5], orbit[4], orbit[5]).iter() {
-                ret.push([*x1, *x2, *y1, *y2, *z1, *z2]);
-            }
-        }
-    }
-    Some(ret)
-}
-
-fn cranges(s1: i32, s2: i32, o1: i32, o2: i32) -> Vec<(i32, i32)> {
-    if s1 <= o1 && o2 <= s2 {
-        vec![(o1, o2)] // maybe?
-    } else if o1 < s1 && s2 < o2 {
-        vec![(o1, s1 - 1), (s1, s2), (s2, o2)]
-    } else if s1 <= o1 {
-        vec![(s2 + 1, o2)]
-    } else {
-        vec![(o1, s1 - 1)]
-    }
-}
-
-fn is_disjointed(s: &[i32; 6], o: &[i32; 6]) -> bool {
-    s[0] > o[1] || s[1] < o[0] || s[2] > o[3] || s[3] < o[2] || s[4] > o[5] || s[5] < o[4]
+fn overlaps(s: &[i32; 6], o: &[i32; 6]) -> bool {
+    !(s[0] > o[1] || s[1] < o[0] || s[2] > o[3] || s[3] < o[2] || s[4] > o[5] || s[5] < o[4])
 }
 
 fn swallows(s: &[i32; 6], o: &[i32; 6]) -> bool {
     s[0] < o[0] && s[1] > o[1] && s[2] < o[2] && s[3] > o[3] && s[4] < o[4] && s[5] > o[5]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_single_point_overlap() {
+        assert_eq!(overlaps(&[0, 5, 0, 5, 0, 5], &[-5, 0, -5, 0, -5, 0]), true);
+    }
+
+    #[test]
+    fn test_line_overlap() {
+        assert_eq!(overlaps(&[0, 5, 0, 5, 0, 5], &[-5, 0, -5, 0, 0, 5]), true);
+    }
+
+    #[test]
+    fn test_side_overlap() {
+        assert_eq!(overlaps(&[0, 5, 0, 5, 0, 5], &[-5, 0, 0, 5, 0, 5]), true);
+    }
+
+    #[test]
+    fn test_basic_overlap() {
+        assert_eq!(overlaps(&[0, 5, 0, 5, 0, 5], &[2, 7, 2, 7, 2, 7]), true);
+    }
+
+    #[test]
+    fn test_straddling_overlap() {
+        assert_eq!(overlaps(&[-1, 7, 2, 4, 2, 4], &[0, 5, 0, 5, 0, 5]), true);
+    }
+
+    #[test]
+    fn test_stabbing_overlap() {
+        assert_eq!(overlaps(&[2, 8, 2, 4, 2, 4], &[0, 5, 0, 5, 0, 5]), true);
+    }
+
+    #[test]
+    fn test_swallowing_overlap() {
+        assert_eq!(overlaps(&[3, 5, 3, 5, 3, 5], &[2, 7, 2, 7, 2, 7]), true);
+    }
+
+    #[test]
+    fn test_basic_non_overlap() {
+        assert_eq!(
+            overlaps(&[0, 5, 0, 5, 0, 5], &[12, 17, 12, 17, 12, 17]),
+            false
+        );
+    }
 }
 
